a3fd2e6c12a6deabb9b8f15235b5ae9459a46cd8
kne <s2019s06@e-mirim.hs.kr>
2021-04-21 10:19:34 +0900

    초기 커밋.

---
+++ CSBasic2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSBasic2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("숫자 입력 : ");
+
+            int input = int.Parse(Console.ReadLine());
+
+            if (input % 2 == 0)
+            {
+                Console.WriteLine("짝수");
+            }
+            if (input % 2 == 1)
+            {
+                Console.WriteLine("홀수");
+            }
+
+            Console.WriteLine(DateTime.Now.Year);
+            Console.WriteLine(DateTime.Now.Month);
+            Console.WriteLine(DateTime.Now.Day);
+        }
+    }
+}
